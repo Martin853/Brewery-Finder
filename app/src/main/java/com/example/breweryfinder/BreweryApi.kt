01dad96f64be308be4574ab7abdf71c3642440dc
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-06-07 19:29:56 +0200

    Created the brewery api interface.

---
+++ app/src/main/java/com/example/breweryfinder/BreweryApi.kt
@@ -0,0 +1,10 @@
+package com.example.breweryfinder
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BreweryApi {
+    @GET("/v1/breweries")
+    fun getBreweries(@Query("per_page") per_page: Int): Response<List<Brewery>>
+}
