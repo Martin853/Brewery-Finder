13b55f6749a30bdba9098ca34f73415134fe64f5
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-06-08 17:15:48 +0200

    Created the BreweryAdapter and created the lifecycle scope.

---
+++ app/src/main/java/com/example/breweryfinder/BreweryAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.breweryfinder
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.breweryfinder.databinding.ActivityMainBinding
+import com.example.breweryfinder.databinding.ItemBreweryBinding
+
+class BreweryAdapter : RecyclerView.Adapter<BreweryAdapter.BreweryViewHolder>() {
+
+    inner class BreweryViewHolder (val binding: ItemBreweryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Brewery>() {
+        override fun areItemsTheSame(oldItem: Brewery, newItem: Brewery): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Brewery, newItem: Brewery): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+    var breweries : MutableList<Brewery>
+        get() = differ.currentList
+        set(value) { differ.submitList(value) }
+
+    override fun getItemCount() = breweries.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BreweryViewHolder {
+        return BreweryViewHolder(ItemBreweryBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+
+    override fun onBindViewHolder(holder: BreweryViewHolder, position: Int) {
+        holder.binding.apply {
+            val brewery = breweries[position]
+            tvTitle.text = brewery.name
+            tvAddressStreet.text = brewery.address_1
+            tvCityState.text = brewery.city
+        }
+    }
+}
--- app/src/main/java/com/example/breweryfinder/MainActivity.kt
+++ app/src/main/java/com/example/breweryfinder/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.breweryfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.breweryfinder.databinding.ActivityMainBinding
+import retrofit2.HttpException
+import java.io.IOException
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var breweryAdapter: BreweryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+
+        lifecycleScope.launchWhenCreated {
+            binding.progressBar.isVisible = true
+            val response = try {
+                RetrofitInstance.api.getBreweries(6)
+            } catch (e: IOException) {
+                Log.e(TAG, "IOException, you might not have internet connection")
+                binding.progressBar.isVisible = false
+                return@launchWhenCreated
+
+            } catch (e: HttpException) {
+                Log.e(TAG, "HttpException, unexcpected response")
+                binding.progressBar.isVisible = false
+                return@launchWhenCreated
+            }
+
+            if (response.isSuccessful && response.body() != null) {
+                breweryAdapter.breweries = (response.body() as MutableList<Brewery>?)!!
+            } else  {
+                Log.e(TAG, "Response not successful")
+            }
+
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    private fun setupRecyclerView() = binding.rvBreweries.apply {
+        breweryAdapter = BreweryAdapter()
+        adapter = breweryAdapter
+        layoutManager = LinearLayoutManager(this@MainActivity)
+
     }
 }
